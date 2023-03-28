480f249d8b0a907409453f2fd6aba8c7b39395d4
Ruei-Lin, Hsu <ray9581@gmail.com>
2023-03-29 00:25:27 +0800

    Update

--- GUIPanel.py
+++ GUIPanel.py
@@ -1,30 +1,50 @@
-# ''' Design the panel '''
-# import tkinter as tk
+''' Import packages '''
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-# ''' Main Programme '''
-# window = tk.Tk()
-# window.title('GUI')
-# window.geometry('360x480')
-# window.resizable(True, True)
-# window.iconbitmap('D:/github/ImageProcessing_GUI/Figures/Icon.bmp')
+''' Functions and Variables '''
+default_dir = "D:/"
 
 
-# def mouseevent
+def ChooseFile():
+    file_path = filedialog.askopenfilename(
+        title=u'choose file', initialdir=(os.path.expanduser(default_dir)))
 
 
-# window.mainloop()
-from tkinter import *
+''' Main Programme '''
+
+# Windows Constructure
+window = tk.Tk()
+window.title('GUI')
+window.geometry('360x480')
+window.resizable(True, True)
+window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.bmp")
+
+# Labels
+menu = tk.Menu(window)
+window.config(menu=menu)
+menu.add_command(label='File', command=ChooseFile)
+
+# Buttons
+file_dir = tk.Button(text='OPEN...', command=ChooseFile)
+file_dir.place(x=0, y=0)
+
+window.mainloop()
+
+# ''' Work Space '''
+# from tkinter import *
 
 
-def motion(event):
-    print("Mouse position: (%s %s)" % (event.x, event.y))
-    return
+# def motion(event):
+#     print("Mouse position: (%s %s)" % (event.x, event.y))
+#     return
 
 
-master = Tk()
-whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.\n(Mahatma Gandhi)"
-msg = Message(master, text=whatever_you_do)
-msg.config(bg='lightgreen', font=('times', 24, 'italic'))
-msg.bind('<Motion>', motion)
-msg.pack()
-mainloop()
+# master = Tk()
+# whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.\n(Mahatma Gandhi)"
+# msg = Message(master, text=whatever_you_do)
+# msg.config(bg='lightgreen', font=('times', 24, 'italic'))
+# msg.bind('<Motion>', motion)
+# msg.pack()
+# mainloop()

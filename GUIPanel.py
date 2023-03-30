d2cfd857a318d3fc86bb3acbe54299c3451e295f
Ruei-Lin, Hsu <ray9581@gmail.com>
2023-03-30 16:13:05 +0800

    Update

--- GUIPanel.py
+++ GUIPanel.py
@@ -2,6 +2,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog
+from PIL import ImageTk, Image
 
 ''' Functions and Variables '''
 default_dir = "D:/"
@@ -9,7 +10,20 @@ default_dir = "D:/"
 
 def ChooseFile():
     file_path = filedialog.askopenfilename(
-        title=u'choose file', initialdir=(os.path.expanduser(default_dir)))
+        title=u'choose file', initialdir=(os.path.expanduser(default_dir)), filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    print(file_path)
+
+
+def SaveFile():
+    print('Saving...')
+
+
+def DumpFile():
+    print('Dumping...')
+
+
+def DisplayImage():
+    print('Display...')
 
 
 ''' Main Programme '''
@@ -17,18 +31,35 @@ def ChooseFile():
 # Windows Constructure
 window = tk.Tk()
 window.title('GUI')
-window.geometry('360x480')
+window.geometry('800x600')
 window.resizable(True, True)
-window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.bmp")
+window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.ico")
+
+# Frames
+frame = tk.Frame(window, width=400, height=300)
+frame.pack()
+frame.place(x=200, y=50)
+
+
+img = ImageTk.PhotoImage(Image.open(
+    'D:/github/ImageProcessing_GUI/Figures/Icon.ico'))
+label = tk.Label(frame, image=img)
+label.pack()
 
 # Labels
-menu = tk.Menu(window)
-window.config(menu=menu)
-menu.add_command(label='File', command=ChooseFile)
+menu1 = tk.Menu(window)
+window.config(menu=menu1)
+
+# Second Layer Labels
+menu2 = tk.Menu(menu1, tearoff=0)
+menu2.add_command(label='Open...', command=ChooseFile)
+menu2.add_command(label='Save...', command=SaveFile)
+menu1.add_cascade(label='File', menu=menu2)
 
 # Buttons
-file_dir = tk.Button(text='OPEN...', command=ChooseFile)
-file_dir.place(x=0, y=0)
+button_Display = tk.Button(text='Diaplay')
+button_Display.place(x=0, y=0)
+button_Display.pack()
 
 window.mainloop()
 

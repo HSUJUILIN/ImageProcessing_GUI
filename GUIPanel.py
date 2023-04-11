096d4dcbd02edf081d701a67dc5123f0f1a43ff3
Ruei-Lin, Hsu <ray9581@gmail.com>
2023-04-11 15:01:37 +0800

    Update

--- GUIPanel.py
+++ GUIPanel.py
@@ -10,8 +10,12 @@ default_dir = "D:/"
 
 def ChooseImage():
     file_path = filedialog.askopenfilename(
-        title=u'choose file', initialdir=(os.path.expanduser(default_dir)), filetypes=(("png files", "*.png"), ("all files", "*.*")))
+        title=u'choose file', initialdir=(os.path.expanduser(default_dir)), filetypes=(("png files", "*.png"), ("tif files", "*.tif"),  ("all files", "*.*")))
     img = ImageTk.PhotoImage(Image.open(file_path))
+    # original_image = img.subsample(2, 2)  # resize image using subsample
+    # tk.Label(Left_frame, image=original_image).grid(
+    #     row=1, column=0, padx=5, pady=5)
+    tk.Label(Right_frame, image=img).grid(row=0, column=0, padx=5, pady=5)
 
 
 def SaveFile():
@@ -29,7 +33,7 @@ window = tk.Tk()
 window.title('GUI')
 window.geometry('800x600')
 window.resizable(True, True)
-window.config(bg='black')
+window.config(bg='white')
 window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.ico")
 
 
@@ -44,8 +48,8 @@ menu2.add_command(label='Save...', command=SaveFile)
 menu1.add_cascade(label='File', menu=menu2)
 
 # Frames
-LeftTop_frame = tk.Frame(window, width=200, height=400)
-LeftTop_frame.grid(row=0, column=0, padx=10, pady=5)
+Left_frame = tk.Frame(window, width=200, height=400)
+Left_frame.grid(row=0, column=0, padx=10, pady=5)
 
 Right_frame = tk.Frame(window, width=550, height=400)
 Right_frame.grid(row=0, column=1, padx=10, pady=5)

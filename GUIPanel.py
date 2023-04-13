8dbfeca99678b27ad5b393c46b3a202f638f78bf
Ruei-Lin, Hsu <ray9581@gmail.com>
2023-04-13 17:28:13 +0800

    Update

--- GUIPanel.py
+++ GUIPanel.py
@@ -21,15 +21,23 @@ def ChooseImage():
         ori_image.thumbnail((140/ori_image.size[1]*ori_image.size[0], 140))
     else:
         pass
+    if (ori_image.size[0] < 550 and ori_image.size[1] < 580):
+        pass
     img = ImageTk.PhotoImage(ori_image)
     SubSample.config(image=img)
     SubSample.image = img
-    StateWindow.insert(0, "Loading image...")
+    StateWindow.insert(1.0, "Loading image...\n\n\n")
 
 
 # def clear():
 #     height.set("")
 #     width.set("")
+def test1():
+    StateWindow.insert(tk.END, "test1\n")
+
+
+def test2():
+    StateWindow.insert(tk.END, "test2\n")
 
 
 def SaveFile():
@@ -62,12 +70,11 @@ window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.ico")
 # menu1.add_cascade(label='File', menu=menu2)
 
 # Frames (put the widgets)
-LeftTop_frame = tk.Frame(window, width=200, height=400)
+LeftTop_frame = tk.Frame(window)
 LeftTop_frame.place(x=5, y=5, height=400, width=200)
-LeftBottom_frame = tk.Frame(window, width=200, height=180)
+LeftBottom_frame = tk.Frame(window)
 LeftBottom_frame.place(x=5, y=410, height=185, width=200)
-
-Right_frame = tk.Frame(window, width=550, height=580)
+Right_frame = tk.Frame(window)
 Right_frame.place(x=210, y=5, height=590, width=585)
 
 # Labels (put the text or image)
@@ -78,28 +85,31 @@ SubSample = tk.Label(LeftTop_frame)
 SubSample.place(x=20, y=70, height=140, width=140)
 InfoSample = tk.Label(LeftTop_frame, text="")
 InfoSample.place(x=100, y=250, anchor="center")
-# SubSample_information = tk.Label(LeftTop_frame, text='Information')
-
-# file_path = filedialog.askopenfilename(
-#     title=u'choose file', initialdir=(os.path.expanduser(default_dir)), filetypes=(("png files", "*.png"), ("tif files", "*.tif"),  ("all files", "*.*")))
-# img = ImageTk.PhotoImage(Image.open(file_path))
-# SubSample = tk.Label(LeftTop_frame, image=img)
-# SubSample.place(anchor='center', relx=0.5, rely=0.5)
-
 
 # Buttons
 Button_LoadImage = tk.Button(
     LeftTop_frame, text='Load...', command=ChooseImage)
 Button_LoadImage.place(x=0, y=40)
 
+Button_test1 = tk.Button(Right_frame, text="test1", command=test1)
+Button_test1.place(relx=0, rely=0.1)
+Button_test2 = tk.Button(Right_frame, text="test2", command=test2)
+Button_test2.place(relx=0, rely=0.2)
+
 # Texts
-StateWindow = tk.Text(LeftTop_frame, height=6)
+StateWindow = tk.Text(LeftTop_frame)
 StateWindow.place(x=10, y=285, relwidth=0.9, height=110)
+
+# ScrollBars
+Bar_State = tk.Scrollbar(StateWindow, orien='vertical',
+                         command=StateWindow.yview)
+Bar_State.pack(side=tk.RIGHT, fill='y')
 
 # Separators
 Separator1 = ttk.Separator(LeftTop_frame, orient='horizontal')
 Separator1.place(relx=0, y=280, relwidth=1, relheight=0.01)
 
+# print(LeftTop_frame.winfo_screenwidth())
 window.mainloop()
 
 # ''' Work Space '''

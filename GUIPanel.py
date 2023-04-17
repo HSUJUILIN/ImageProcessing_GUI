4b5b6b3a0f73f1ae403c1a357318250c27e743cf
Ruei-Lin, Hsu <ray9581@gmail.com>
2023-04-17 15:22:29 +0800

    Update

--- GUIPanel.py
+++ GUIPanel.py
@@ -2,13 +2,16 @@
 import os
 import tkinter as tk
 from PIL import ImageTk, Image
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk, messagebox
 
 # ''' Functions and Variables '''
 default_dir = "D:/"
+StateCount = 0
 
 
 def ChooseImage():
+    global StateCount
+
     file_path = filedialog.askopenfilename(
         title=u'choose file', initialdir=(os.path.expanduser(default_dir)), filetypes=(("png files", "*.png"), ("tif files", "*.tif"),  ("all files", "*.*")))
     filename = os.path.basename(file_path)
@@ -26,18 +29,49 @@ def ChooseImage():
     img = ImageTk.PhotoImage(ori_image)
     SubSample.config(image=img)
     SubSample.image = img
-    StateWindow.insert(1.0, "Loading image...\n\n\n")
+    Text_StateWindow.insert(tk.END, "Loading image...\n")
+    Text_StateWindow.see(tk.END)
+    StateCount += 1
+
+    # def clear():
+    #     height.set("")
+    #     width.set("")
 
 
-# def clear():
-#     height.set("")
-#     width.set("")
 def test1():
-    StateWindow.insert(tk.END, "test1\n")
+    global StateCount
+
+    Text_StateWindow.insert(tk.END, "test1\n")
+    Text_StateWindow.see(tk.END)
+    StateCount += 1
 
 
 def test2():
-    StateWindow.insert(tk.END, "test2\n")
+    global StateCount
+
+    Text_StateWindow.insert(tk.END, "test2\n")
+    Text_StateWindow.see(tk.END)
+    StateCount += 1
+
+
+def Quit():
+    Messagebox_Quit = messagebox.askquestion(
+        'WARNING...', 'Are you sure you want to quit the GUI programe?', icon='warning')
+    if (Messagebox_Quit == 'yes'):
+        window.destroy()
+    else:
+        messagebox.showinfo('RETURN', 'You will return the main window.')
+
+
+def Tutorial():
+    Tutorial = tk.Toplevel(window)
+    Tutorial.title('Tutorial')
+    Tutorial.geometry('500x500')
+    Text_Tutorial = tk.Text(Tutorial)
+    Text_Tutorial.place(x=5, y=5, width=490, height=490)
+    Text_Tutorial.insert(
+        tk.END, 'This is the tutorial of this GUI program.\n\n====================================================================\n\n1. The main Page:\n\n    (1)Image information:There is a preview window\n\n    (2)Window information:')
+    Text_Tutorial.config(state=tk.DISABLED)
 
 
 def SaveFile():
@@ -50,67 +84,73 @@ def DumpFile():
 
 ''' Main Programme '''
 
-# Windows Constructure
-window = tk.Tk()
-window.title('Image Processing GUI')
-window.geometry('800x600')
-window.resizable(True, True)
-window.config(bg='grey')
-window.iconbitmap("D:/github/ImageProcessing_GUI/Figures/Icon.ico")
+if __name__ == "__main__":
+    # Windows Constructure
+    window = tk.Tk()
+    window.title('Image Processing GUI')
+    window.geometry('800x600')
+    window.resizable(True, True)
+    window.config(bg='grey')
 
+    # Ticks (the ticks on the top of window)
+    menu1 = tk.Menu(window)
+    window.config(menu=menu1)
 
-# Ticks (the ticks on the top of window)
-# menu1 = tk.Menu(window)
-# window.config(menu=menu1)
+    # Second Layer Ticks
+    File_menu2 = tk.Menu(menu1, tearoff=0)
+    File_menu2.add_command(label='Open...')
+    File_menu2.add_command(label='Save...')
+    menu1.add_cascade(label='File', menu=File_menu2)
 
-# Second Layer Ticks
-# menu2 = tk.Menu(menu1, tearoff=0)
-# menu2.add_command(label='Open...', command=ChooseImage)
-# menu2.add_command(label='Save...', command=SaveFile)
-# menu1.add_cascade(label='File', menu=menu2)
+    Window_menu2 = tk.Menu(menu1, tearoff=0)
+    Window_menu2.add_command(label='Tutorial', command=Tutorial)
+    Window_menu2.add_command(label='Quit', command=Quit)
+    menu1.add_cascade(label='Window', menu=Window_menu2)
 
-# Frames (put the widgets)
-LeftTop_frame = tk.Frame(window)
-LeftTop_frame.place(x=5, y=5, height=400, width=200)
-LeftBottom_frame = tk.Frame(window)
-LeftBottom_frame.place(x=5, y=410, height=185, width=200)
-Right_frame = tk.Frame(window)
-Right_frame.place(x=210, y=5, height=590, width=585)
+    # Frames (put the widgets)
+    LeftTop_frame = tk.Frame(window)
+    LeftTop_frame.place(x=5, y=5, height=400, width=200)
+    LeftBottom_frame = tk.Frame(window)
+    LeftBottom_frame.place(x=5, y=410, height=185, width=200)
+    Right_frame = tk.Frame(window)
+    Right_frame.place(x=210, y=5, height=590, width=585)
 
-# Labels (put the text or image)
-SubSample_title = tk.Label(
-    LeftTop_frame, text='Image Information', font=("Arial", 15))
-SubSample_title.place(y=0, width=180)
-SubSample = tk.Label(LeftTop_frame)
-SubSample.place(x=20, y=70, height=140, width=140)
-InfoSample = tk.Label(LeftTop_frame, text="")
-InfoSample.place(x=100, y=250, anchor="center")
+    # Labels (put the text or image)
+    SubSample_title = tk.Label(
+        LeftTop_frame, text='Image Information', font=("Arial", 15))
+    SubSample_title.place(y=0, width=180)
+    SubSample = tk.Label(LeftTop_frame)
+    SubSample.place(x=20, y=70, height=140, width=140)
+    InfoSample = tk.Label(LeftTop_frame, text="")
+    InfoSample.place(x=100, y=250, anchor="center")
 
-# Buttons
-Button_LoadImage = tk.Button(
-    LeftTop_frame, text='Load...', command=ChooseImage)
-Button_LoadImage.place(x=0, y=40)
+    # Buttons
+    Button_LoadImage = tk.Button(
+        LeftTop_frame, text='Load...', command=ChooseImage)
+    Button_LoadImage.place(x=0, y=40)
+    Button_Quit = tk.Button(LeftTop_frame, text='Quit',
+                            command=Quit)
+    Button_Quit.place(x=160, y=40)
 
-Button_test1 = tk.Button(Right_frame, text="test1", command=test1)
-Button_test1.place(relx=0, rely=0.1)
-Button_test2 = tk.Button(Right_frame, text="test2", command=test2)
-Button_test2.place(relx=0, rely=0.2)
+    Button_test1 = tk.Button(Right_frame, text="test1", command=test1)
+    Button_test1.place(relx=0, rely=0.1)
+    Button_test2 = tk.Button(Right_frame, text="test2", command=test2)
+    Button_test2.place(relx=0, rely=0.2)
 
-# Texts
-StateWindow = tk.Text(LeftTop_frame)
-StateWindow.place(x=10, y=285, relwidth=0.9, height=110)
+    # Texts
+    Text_StateWindow = tk.Text(LeftTop_frame)
+    Text_StateWindow.place(x=10, y=285, relwidth=0.9, height=110)
 
-# ScrollBars
-Bar_State = tk.Scrollbar(StateWindow, orien='vertical',
-                         command=StateWindow.yview)
-Bar_State.pack(side=tk.RIGHT, fill='y')
+    # ScrollBars
+    Bar_State = tk.Scrollbar(Text_StateWindow, orien='vertical',
+                             command=Text_StateWindow.yview)
+    Bar_State.pack(side=tk.RIGHT, fill='y')
 
-# Separators
-Separator1 = ttk.Separator(LeftTop_frame, orient='horizontal')
-Separator1.place(relx=0, y=280, relwidth=1, relheight=0.01)
+    # Separators
+    Separator1 = ttk.Separator(LeftTop_frame, orient='horizontal')
+    Separator1.place(relx=0, y=280, relwidth=1, relheight=0.01)
 
-# print(LeftTop_frame.winfo_screenwidth())
-window.mainloop()
+    window.mainloop()
 
 # ''' Work Space '''
 
